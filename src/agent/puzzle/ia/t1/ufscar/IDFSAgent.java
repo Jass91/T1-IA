60dd7fa229adc2bf75d0fefa9dfb5853bd6870a0
Jesley Marcelino <jesley.caceres.marcelino@gmail.com>
2016-10-04 12:19:01 -0300

    agente de busca iterativa resolvido

--- src/agent/puzzle/ia/t1/ufscar/IDFSAgent.java
+++ src/agent/puzzle/ia/t1/ufscar/IDFSAgent.java
@@ -1,13 +1,12 @@
 package agent.puzzle.ia.t1.ufscar;
 
 import java.util.List;
-
 import border.agent.puzzle.ia.t1.ufscar.StackBorder;
 import game.puzzle.ia.t1.ufscar.GameState;
-import general.agent.puzzle.ia.t1.ufscar.GraphAgent;
+import general.agent.puzzle.ia.t1.ufscar.TreeAgent;
 
 //agente que executa uma busca em profundidade iterativa
-public class IDFSAgent extends GraphAgent{
+public class IDFSAgent extends TreeAgent{
 
 	private int maxLimit;
 	private int depth;
@@ -21,46 +20,43 @@ public class IDFSAgent extends GraphAgent{
 
 	}
 	
-	// sobrescreve o metodo de resolucao
+	/*
+	 * Sobrescreve o metodo de resolucao,
+	 * pois esse agente executa n = 1, 2, ..., maxLimit
+	 * 
+	 */
 	@Override
 	public List<GameState> resolve(){
 
 		int totalOfExploredNodes = 0;
 		int totalOfGeneratedNodes = 0;
 		
-		// utiliza o agente de busca em profundidade limitada
-		LDFSAgent agent = new LDFSAgent(initialState, problemSize, depth);
-		
 		// aumenta o limite de depth ate maxLimit gradativamente
 		while(depth <= maxLimit){
 			
-			List<GameState> resolution = agent.resolve();
+			List<GameState> resolution = super.resolve();
 			
 			// atualiza as variaveis (para cada busca conta os estados gerados e explorados)
-			totalOfExploredNodes += agent.getNumberOfExploredNodes();
-			totalOfGeneratedNodes += agent.getNumberOfGeneratedNodes();
+			totalOfExploredNodes += numberOfExploredNodes;
+			totalOfGeneratedNodes += numberOfGeneratedNodes;
 			
 			if(resolution != null){
-				
-				this.goalState = agent.getGoalState();
-				this.numberOfExploredNodes = totalOfExploredNodes;
-				this.numberOfGeneratedNodes = totalOfGeneratedNodes;
-				
+				numberOfExploredNodes = totalOfExploredNodes;
+				numberOfGeneratedNodes = totalOfGeneratedNodes;
 				return resolution;
 			}
 			
 			// muda a profundidade limite
-			agent.setLimit(++depth);
+			depth++;
 		}
 		
-		// monta as informacoes 
-		this.goalState = agent.getGoalState();
-		this.numberOfExploredNodes = totalOfExploredNodes;
-		this.numberOfGeneratedNodes = totalOfGeneratedNodes;
+		numberOfExploredNodes = totalOfExploredNodes;
+		numberOfGeneratedNodes = totalOfGeneratedNodes;
 		
 		return null;
 	}
 
+	
 	// Sobrescreve a funcao que expande o estado.
 	// De acordo com o comportamento geral desse tipo de agente,
 	// o estado sera expandido apenas se n.depth < limit
